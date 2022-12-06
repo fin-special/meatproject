4f82da4515ba034bcb42036ad00fef2231a334b8
AsherInstinct <bingorkdls@gmail.com>
2022-12-06 20:33:19 +0900

    HTML structure fixed

--- pybo/functions.py
+++ pybo/functions.py
@@ -1,6 +1,8 @@
 
 import json
 
+from sqlalchemy import func
+
 
 def predict_price(filepath, column):
     meat_sum = 0
@@ -11,3 +13,27 @@ def predict_price(filepath, column):
         meat_sum += data[i][column]
     meat_price = int(meat_sum/7)
     return meat_price
+
+
+# 조류독감 밯병 갯수
+def GetAiCnt(model):
+    data = model.query.filter(model.date >= func.ADDDATE(func.NOW(), -30)).filter(model.ds_nm.like('고병원성%')).count()
+    
+    return data
+
+# 조류독감 발병현황
+def GetAiData(model):
+    data = model.query.filter(model.ds_nm.like('고병원성%')).filter(model.date >= func.ADDDATE(func.NOW(), -30)).order_by(model.date.desc()).limit(5)
+    return data
+
+# 소, 돼지 발병 갯수
+def GetCnt(model, keyword):
+    data = model.query.filter(model.date >= func.ADDDATE(func.NOW(), -30)).filter(model.animal.like(f'{keyword}%')).count()
+    
+    return data
+
+# 소, 돼지 발병현황
+def GetData(model, keyword):
+    data = model.query.filter(model.animal.like(f'{keyword}%')).filter(model.date >= func.ADDDATE(func.NOW(), -30)).order_by(model.date.desc()).limit(5)
+    return data
+
--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -2,15 +2,15 @@ from flask import Blueprint, render_template, url_for
 from ..models import Newschicken, Newscow, Newspork, Disease_current
 from sqlalchemy import func
 import json
-from pybo.functions import predict_price
+from pybo.functions import predict_price, GetAiCnt, GetAiData, GetCnt, GetData
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
-# 메인화면
 
 
-@bp.route('/')
-def home():
+# 메인페이지------------------------------------------------------------------------------------
+@bp.route('/',  methods=["GET"])
+def main():
 
     chicken_path = "pybo\static\json\chicken_predict_price.json"
     cow_path = "pybo\static\json\cow_predict_price.json"
@@ -20,30 +20,59 @@ def home():
     cow_price = predict_price(cow_path, 'yhat')
     pork_price = predict_price(pork_path, 'yhat')
 
-    return render_template('index.html', chicken_price=chicken_price, cow_price=cow_price, pork_price=pork_price)
-
-# 닭고기 가격 예측 api
+    return render_template('base_index.html', chicken_price=chicken_price, cow_price=cow_price, pork_price=pork_price)
 
 
+
+# 닭고기 페이지------------------------------------------------------------------------------------
+@bp.route('/chicken',  methods=["GET"])
+def base_generic1():
+    case_cnt = GetAiCnt(Disease_current)
+    showfive = GetAiData(Disease_current)
+    
+    return render_template('base_generic1.html', showfive=showfive, case_cnt=case_cnt)
+
+
+
+# 소고기 페이지------------------------------------------------------------------------------------
+@bp.route('/cow',  methods=["GET"])
+def base_generic2():
+    case_cnt = GetCnt(Disease_current,'소')
+    showfive = GetData(Disease_current,'소')
+   
+
+    return render_template('base_generic2.html', showfive=showfive, case_cnt=case_cnt)
+
+
+
+# 돼지고기 페이지------------------------------------------------------------------------------------
+@bp.route('/pig',  methods=["GET"])
+def base_generic3():
+    case_cnt = GetCnt(Disease_current,'돼지')
+    showfive = GetData(Disease_current,'돼지')
+
+    return render_template('base_generic3.html', showfive=showfive, case_cnt=case_cnt)
+
+
+
+
+
+# 닭고기 뉴스
 @bp.route('/predictchicken', methods=['GET'])
 def predictChicken():
     news_chicken = Newschicken.query.all()
 
     # return render_template('predictChicken.html', news_chicken=news_chicken)
-    return render_template('generic.html', news_chicken=news_chicken)
+    return render_template('predictChicken.html', news_chicken=news_chicken)
 
-# 소고기 가격 예측 api
-
-
+# 소고기 뉴스
 @bp.route('/predictcow', methods=['GET'])
 def predictCow():
     news_cow = Newscow.query.all()
 
     return render_template('predictCow.html', news_cow=news_cow)
 
-# 돼지고기 가격 예측 api
-
-
+# 돼지고기 뉴스
 @bp.route('/predictpork', methods=['GET'])
 def predictPork():
     news_pork = Newspork.query.all()
@@ -51,25 +80,9 @@ def predictPork():
     return render_template('predictPork.html', news_pork=news_pork)
 
 
-@bp.route('/ai_current_css', methods=["GET"])
-def ai_current_css():
-    case_cnt = Disease_current.query.filter(
-        Disease_current.date >= func.ADDDATE(func.NOW(), -30)).count()
-    showfive = Disease_current.query.filter(Disease_current.animal.like(
-        '닭%')).order_by(Disease_current.date.desc()).limit(5)
-    return render_template('disease_current_css.html', showfive=showfive, case_cnt=case_cnt)
 
 
-@bp.route('/base_test1',  methods=["GET"])
-def base_test1():
-
-    return render_template('base_test1.html')
-
-
-@bp.route('/base_test2',  methods=["GET"])
-def base_test2():
-    case_cnt = Disease_current.query.filter(
-        Disease_current.date >= func.ADDDATE(func.NOW(), -30)).count()
-    showfive = Disease_current.query.filter(Disease_current.animal.like(
-        '닭%')).order_by(Disease_current.date.desc()).limit(5)
-    return render_template('base_test2.html', showfive=showfive, case_cnt=case_cnt)
+# 백엔드에서 base.html 접근용
+@bp.route('/base', methods=["GET"])
+def base():
+    return render_template('base.html')
